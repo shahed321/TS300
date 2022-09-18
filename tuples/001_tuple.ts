5519ece0f2310ff624e63526b743eb477b8145a8
Shahed Ahmed Akkagi <sabuj.321@outlook.com>
2022-09-18 21:53:53 +0600

    tuples

---
+++ tuples/001_tuple.ts
@@ -0,0 +1,7 @@
+// tuples are pre-defined length and types of array
+let ourTuple: [number, boolean, string];
+
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
+
+console.log(ourTuple);
