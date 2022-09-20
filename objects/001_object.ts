2a8a50f5d9d18593a8f295f7025ea403db4a342b
Shahed Ahmed Akkagi <sabuj.321@outlook.com>
2022-09-20 16:04:17 +0600

    objects 001

---
+++ objects/001_object.ts
@@ -0,0 +1,8 @@
+// Try playing around with modifying properties and adding ones to see what happens
+const car: { type: string, model: string, year: number } = {
+    type: "Toyota",
+    model: "Corolla",
+    year: 2009
+  };
+  
+  console.log(car);
